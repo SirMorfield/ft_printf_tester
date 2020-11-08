126780ded5134d9192d56e724ae7c62eb0292467
SirMorfield <joppe.koers@gmail.com>
2020-11-09 00:46:23 +0100

    added --output tag and improved errpr handling

--- app.js
+++ app.js
@@ -6,35 +6,73 @@
 /*   By: jkoers <jkoers@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/11/04 19:31:54 by jkoers        #+#    #+#                 */
-/*   Updated: 2020/11/07 16:42:39 by jkoers        ########   odam.nl         */
+/*   Updated: 2020/11/08 23:28:16 by jkoers        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
+const build_cmd = 'make -C ../ft_printf/'
 const ft_bin = '../ft_printf/bin/'
 const ft_header = '../ft_printf/'
 
 const testCases = require('./testcases.js')
 const { execSync, exec } = require('child_process')
-const fs = require('fs').promises
 
 function color(text, r, g, b) {
 	return ("\033[38;2;" + `${r};${g};${b}m${text}` + "\033[0m")
 }
 
+try {
+	console.log(color((execSync(build_cmd, { stdio: 'pipe' })).toString(), 105, 105, 105));
+} catch (err) {
+	process.stdout.write(err.stdout.toString())
+	process.stdout.write(err.stderr.toString())
+}
+
+
+if (process.argv[2] == '--output') {
+	let out
+	console.log(`Testing (${process.argv[3]}) `)
+	try {
+		execSync(`gcc -w -DTESTCASE='${process.argv[3]}' -DFT ./runner/run_tests.c -L${ft_bin} -I${ft_header} -lftprintf -o runner/ft_printf`, { stdio: 'pipe' })
+		out = (execSync(`./runner/ft_printf`, { stdio: 'pipe' })).toString()
+	} catch (err) {
+		process.stdout.write(err.stdout.toString())
+		process.stdout.write(err.stderr.toString())
+	}
+	if (out) {
+		console.log(out)
+	}
+	return
+}
+
 (async () => {
 	for (const testCase of testCases) {
+		process.stdout.write(`Testing (${testCase}) `)
 		execSync(`gcc -w -DTESTCASE='${testCase}' ./runner/run_tests.c -o runner/printf`)
 		execSync(`gcc -w -DTESTCASE='${testCase}' -DFT ./runner/run_tests.c -L${ft_bin} -I${ft_header} -lftprintf -o runner/ft_printf`)
-		const printf_output = (execSync(`./runner/printf`)).toString()
-		const ft_printf_output = (execSync(`./runner/ft_printf`)).toString()
+		let printf_output
+		let ft_printf_output
+		try {
+			printf_output = (execSync(`./runner/printf`, { stdio: 'pipe' })).toString()
+		} catch (err) {
+			printf_output = `${color('Execution error', 255, 0, 0)}`
+		}
+		try {
+			ft_printf_output = (execSync(`./runner/ft_printf`, { stdio: 'pipe' })).toString()
+		} catch (err) {
+			ft_printf_output = `${color('Execution error', 255, 0, 0)}`
+		}
 
 		if (printf_output != ft_printf_output) {
-			console.log(`${color('[KO]', 255, 0, 0)} ft_printf(${testCase})`)
-			console.log(`printf:        <${printf_output}>`)
-			console.log(`ft_printf:     <${ft_printf_output}>\n`)
+			console.log(`${color('[KO]', 255, 0, 0)}`)
+			console.log(`printf:    <${printf_output}>`)
+			console.log(`ft_printf: <${ft_printf_output}>`)
 		}
 		else {
-			console.log(`${color('[OK]', 0, 255, 0)} ft_printf(${testCase})`)
+			console.log(`${color('[OK]', 0, 255, 0)}`)
+			console.log(`<${color(printf_output, 0, 255, 0)}>`)
 		}
+		console.log("");
+
 	}
 })()

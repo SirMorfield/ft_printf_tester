22ca9e6bb2ec8393882d4e81e727a3d86c1de90c
Marius Van wijk <mvan-wij@f0r4s6.codam.nl>
2020-11-09 15:38:55 +0100

    y

--- app.js
+++ app.js
@@ -6,7 +6,7 @@
 /*   By: jkoers <jkoers@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/11/04 19:31:54 by jkoers        #+#    #+#                 */
-/*   Updated: 2020/11/08 23:28:16 by jkoers        ########   odam.nl         */
+/*   Updated: 2020/11/09 15:37:40 by mvan-wij      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,18 +22,19 @@ function color(text, r, g, b) {
 }
 
 try {
-	console.log(color((execSync(build_cmd, { stdio: 'pipe' })).toString(), 105, 105, 105));
+	console.log(color((execSync(build_cmd, { stdio: 'pipe' })).toString(), 105, 105, 105))
 } catch (err) {
 	process.stdout.write(err.stdout.toString())
 	process.stdout.write(err.stderr.toString())
 }
 
-
-if (process.argv[2] == '--output') {
+opt_output = process.argv.indexOf('--output')
+opt_only_ko = process.argv.indexOf('--only-ko')
+if (opt_output != -1) {
 	let out
-	console.log(`Testing (${process.argv[3]}) `)
+	console.log(`Testing (${opt_output + 1}) `)
 	try {
-		execSync(`gcc -w -DTESTCASE='${process.argv[3]}' -DFT ./runner/run_tests.c -L${ft_bin} -I${ft_header} -lftprintf -o runner/ft_printf`, { stdio: 'pipe' })
+		execSync(`gcc -w -DTESTCASE='${opt_output + 1}' -DFT ./runner/run_tests.c -L${ft_bin} -I${ft_header} -lftprintf -o runner/ft_printf`, { stdio: 'pipe' })
 		out = (execSync(`./runner/ft_printf`, { stdio: 'pipe' })).toString()
 	} catch (err) {
 		process.stdout.write(err.stdout.toString())
@@ -46,10 +47,11 @@ if (process.argv[2] == '--output') {
 }
 
 (async () => {
+	let has_ko = false
 	for (const testCase of testCases) {
 		process.stdout.write(`Testing (${testCase}) `)
-		execSync(`gcc -w -DTESTCASE='${testCase}' ./runner/run_tests.c -o runner/printf`)
-		execSync(`gcc -w -DTESTCASE='${testCase}' -DFT ./runner/run_tests.c -L${ft_bin} -I${ft_header} -lftprintf -o runner/ft_printf`)
+		execSync(`gcc -w -DTESTCASE="${testCase.replace(/"/g, `\\"`)}" ./runner/run_tests.c -o runner/printf`)
+		execSync(`gcc -w -DTESTCASE="${testCase.replace(/"/g, `\\"`)}" -DFT ./runner/run_tests.c -L${ft_bin} -I${ft_header} -lftprintf -o runner/ft_printf`)
 		let printf_output
 		let ft_printf_output
 		try {
@@ -64,15 +66,20 @@ if (process.argv[2] == '--output') {
 		}
 
 		if (printf_output != ft_printf_output) {
+			has_ko = true
 			console.log(`${color('[KO]', 255, 0, 0)}`)
 			console.log(`printf:    <${printf_output}>`)
 			console.log(`ft_printf: <${ft_printf_output}>`)
 		}
-		else {
+		else if (opt_only_ko == -1) {
 			console.log(`${color('[OK]', 0, 255, 0)}`)
 			console.log(`<${color(printf_output, 0, 255, 0)}>`)
 		}
-		console.log("");
-
+		console.log("")
+	}
+	if (has_ko) {
+		console.log(`${color('[KO]', 255, 0, 0)}`)
+	} else {
+		console.log(`${color('[OK]', 0, 255, 0)}`)
 	}
 })()
